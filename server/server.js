08c5264d1a26c0587686a9ff8e9b578827521294
kvammee <elliott.kvamme@gmail.com>
2022-01-20 10:20:31 -0600

    Incorporated Apollo Server into existing server for parallel development

--- server/server.js
+++ server/server.js
@@ -2,9 +2,24 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers
+  });
+
+  await server.start();
+
+  server.applyMiddleware(app);
+
+  console.log(`GraphQL endpoint ==> http://localhost:${PORT}${server.graphqlPath}`);
+}
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
